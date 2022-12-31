e5a4e5d502f964447bb336c94adbe7a853945a0e
mayman <m.ayman@tamkeentech.sa>
2022-12-31 03:33:11 +0300

    add annotation

--- account/src/main/java/com/anchtun/account/repository/AccountRepository.java
+++ account/src/main/java/com/anchtun/account/repository/AccountRepository.java
@@ -1,9 +1,11 @@
 package com.anchtun.account.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.anchtun.account.model.Account;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	
 	Account findByCustomerId(Long customerId);
