1959ae4753fc086761ec125ffc14c2d67b408843
m.ayman <m.ayman@tamkeentech.sa>
2023-01-25 22:26:34 +0300

    encrypt-refreshscope

--- account/src/main/java/com/anchtun/account/AccountApplication.java
+++ account/src/main/java/com/anchtun/account/AccountApplication.java
@@ -3,11 +3,13 @@ package com.anchtun.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.anchtun.account.controller") })
 @EnableJpaRepositories("com.anchtun.account.repository")
 @EntityScan("com.anchtun.account.model")
--- loan/src/main/java/com/anchtun/loan/LoanApplication.java
+++ loan/src/main/java/com/anchtun/loan/LoanApplication.java
@@ -3,11 +3,13 @@ package com.anchtun.loan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.anchtun.loan.controller") })
 @EnableJpaRepositories("com.anchtun.loan.repository")
 @EntityScan("com.anchtun.loan.model")
