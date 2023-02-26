8670c8a84d4479e16b747891fde8f577acbab0d1
m.ayman <m.ayman@tamkeentech.sa>
2023-02-26 21:14:15 +0300

    log tracing

--- gatewayserver/src/main/java/com/anchtun/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/anchtun/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,17 @@ package com.anchtun.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import com.anchtun.gatewayserver.trace.logging.ObservationContextSnapshotLifter;
+
+import io.micrometer.context.ContextSnapshot;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Operators;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +21,12 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@ConditionalOnClass({ContextSnapshot.class, Hooks.class})
+	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+	@Bean
+	public ApplicationListener<ContextRefreshedEvent> reactiveObservableHook() {
+		return event -> Hooks.onEachOperator(
+				ObservationContextSnapshotLifter.class.getSimpleName(),
+				Operators.lift(ObservationContextSnapshotLifter.lifter()));
+	}
 }
